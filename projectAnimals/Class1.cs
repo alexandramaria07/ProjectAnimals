c95f7673ddc01760529c5b0273b8f4669b3dd006
alexandra <alexandra_mariaa27@yahoo.com>
2019-06-23 09:19:19 +0300

    project

---
+++ projectAnimals/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Animal
+    {
+        public string Name;
+        public string Sound;
+        public void MakeSound()
+        {
+            Console.WriteLine(this.Name + " makes " + this.Sound);
+        }
+        public Animal(string animalName)
+        {
+            this.Name = animalName;
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog(string dogName) : base(dogName)
+        {
+            this.Sound = "Woof-wooof-wooof";
+        }
+    }
+    class Cat : Animal
+    {
+        public Cat(string catName) : base(catName)
+        {
+            this.Sound = "Meooooooow-meeeowww";
+        }
+    }
+    class Horse : Animal
+    {
+        public Horse(string horseName)
+            : base(horseName)
+        {
+            this.Sound = "Ni-ha-haaa";
+        }
+    }
+    class Pig : Animal
+    {
+        public Pig(string pigName)
+            : base(pigName)
+        {
+            this.Sound = "Oink-oink-oink";
+        }
+        class Cat : Animal
+        {
+            public Cat(string catName) : base(catName)
+            {
+                this.Sound = "Meooooooow-meeeowww";
+            }
+
+        }
+        class Horse : Animal
+
+        {
+            public Horse(string horseName)
+                : base(horseName)
+            {
+                this.Sound = "Ni-ha-haaa";
+            }
+        }
+    }
+}
---
+++ projectAnimals/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[] { new Dog("Mickeeey"), new Cat("Garfield"), new Horse("Patrick"), new Pig("Porky"), };
+            foreach (Animal animal in animals)
+            {
+                animal.MakeSound();
+
+            }
+
+        }
+
+    }
+}
